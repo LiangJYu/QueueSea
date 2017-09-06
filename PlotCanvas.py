73428c7ad9947eae4c84fd6ea07f5b6d8c07011f
Liang Yu <liang.j.yu@gmail.com>
2017-09-06 16:40:16 -0700

    added crude plotting with random data

---
+++ PlotCanvas.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+"""
+QSee functionality replacement
+
+Author: Liang Yu
+Last edited: September 2017
+TODO: better documentation
+"""
+
+import random
+from PyQt5.QtWidgets import QSizePolicy
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+class PlotCanvas(FigureCanvasQTAgg):
+    """
+    where pretty plots go
+    """
+    def __init__(self, parent=None, width=4, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+ 
+        FigureCanvasQTAgg.__init__(self, fig)
+        self.setParent(parent)
+ 
+        FigureCanvasQTAgg.setSizePolicy(self,
+                QSizePolicy.Expanding,
+                QSizePolicy.Expanding)
+        FigureCanvasQTAgg.updateGeometry(self)
+        self.plot()
+ 
+ 
+    def plot(self):
+        """
+        draw stuff
+        """
+        data = [random.random() for i in range(25)]
+        ax = self.figure.add_subplot(111)
+        ax.plot(data, 'r-')
+        ax.set_title('PyQt Matplotlib Example')
+        ax.grid()
+        self.draw()
--- gui.py
+++ gui.py
@@ -5,7 +5,6 @@
 QSee functionality replacement
 
 Author: Liang Yu
-Website: zetcode.com
 Last edited: September 2017
 TODO: do better documentation
 """
@@ -14,6 +13,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from window import Ui_MainWindow
 from DataLoader import DataLoader
+from PlotCanvas import PlotCanvas
 
 class QseeGui(QMainWindow, Ui_MainWindow):
     """
@@ -22,15 +22,23 @@ class QseeGui(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(QseeGui, self).__init__()
         self.setupUi(self)
+        self.init()
 
         #
         self.file_name = ''
         self.data_loader = DataLoader()
 
+    def init(self):
+        """
+        additional init missing from setupUI()
+        """
         # connect buttons
         self.data_select_pushButton.clicked.connect(self.file_load_popup)
         self.data_load_pushButton.clicked.connect(self.load_file)
         self.data_clear_pushButton.clicked.connect(self.clear_loaded_data)
+
+        # setup plot
+        plot_canvas = PlotCanvas(self.plot_widget)
 
     def file_load_popup(self):
         """
