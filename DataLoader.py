77fc144960133cbf98f2e6ff77c2819b5606f071
Liang Yu <liang.j.yu@gmail.com>
2017-09-06 20:16:19 -0700

    bug fixes in Excel loading

--- DataLoader.py
+++ DataLoader.py
@@ -30,10 +30,10 @@ class DataLoader(object):
         temp_data_fields = list(self.dataframe)
 
         # assume there exists only 1 column for time
-        self.time_key = [x for x in temp_data_fields if 'date' in x.lower() or 'time' in x.lower()]
+        self.time_key = [x for x in temp_data_fields if 'date' in x.lower() or 'time' in x.lower()][0]
         # remove time key from data_fields
         if self.time_key:
-            temp_data_fields.remove[self.time_key]
+            temp_data_fields.remove(self.time_key)
 
         self.data_fields.extend(temp_data_fields)
 
